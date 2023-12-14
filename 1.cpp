c51be50d9b9895ae26ebde1050b42500894bdabd
LuthfiMayoga44 <luthfimayoga.gmail.com>
2023-12-14 20:27:38 +0700

    hallo

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int a,b,tambah;
+	
+	cout<<"angka pertama: ";
+	cin>> a;
+	cout<<"angka kedua: ";
+	cin>> b;
+	cout<<"\nhasil perhitungan: ";
+	
+	tambah = a + b;
+	cout<<"hasil dari tambah: "<<tambah;
+	
+	return 0;
+}
